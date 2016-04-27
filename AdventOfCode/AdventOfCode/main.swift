7c69f79011aaf17eff93092ca93b06132d06a8db
Robbie Plankenhorn <rplankenhorn@gmail.com>
2016-04-27 10:52:18 -0500

    Added initial project.

---
+++ AdventOfCode/AdventOfCode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdventOfCode
+//
+//  Created by Robbie Plankenhorn on 4/27/16.
+//  Copyright © 2016 Plankenhorn.net. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
