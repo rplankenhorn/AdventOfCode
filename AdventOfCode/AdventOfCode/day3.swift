490102783726c4371b6629cc1fb0a854ff24b751
Robbie Plankenhorn <rplankenhorn@gmail.com>
2016-04-27 15:33:50 -0500

    Completed Day 3 Part 1

---
+++ AdventOfCode/AdventOfCode/day3.swift
@@ -0,0 +1,63 @@
+//
+//  day3.swift
+//  AdventOfCode
+//
+//  Created by Robbie Plankenhorn on 4/27/16.
+//  Copyright © 2016 Plankenhorn.net. All rights reserved.
+//
+
+import Foundation
+
+enum Direction : String {
+    case North = "^"
+    case South = "v"
+    case East = ">"
+    case West = "<"
+}
+
+struct Point {
+    var x:Int
+    var y:Int
+    
+    init(x:Int, y:Int) {
+        self.x = x
+        self.y = y
+    }
+    
+    func move(direction:Direction) -> Point {
+        switch direction {
+        case .North: return Point(x: self.x, y: self.y-1)
+        case .South: return Point(x: self.x, y: self.y+1)
+        case .East: return Point(x: self.x+1, y: self.y)
+        case .West: return Point(x: self.x-1, y: self.y)
+        }
+    }
+}
+
+extension Point: Hashable {
+    var hashValue: Int {
+        return self.x.hashValue + self.y.hashValue
+    }
+}
+
+func ==(lhs: Point, rhs: Point) -> Bool {
+    return lhs.x == rhs.x && lhs.y == rhs.y
+}
+
+func directions() -> [Direction] {
+    let path = NSBundle.mainBundle().pathForResource("day3_input", ofType: "txt")
+    let text = try? NSString(contentsOfFile: path! as String, encoding: NSUTF8StringEncoding)
+    return String(text!).characters.map { Direction(rawValue: String($0))! }
+}
+
+func day3Part1() -> Int {
+    var currentLocation = Point(x: 0, y: 0)
+    var placesVisited = Set<Point>([currentLocation])
+    
+    for direction in directions() {
+        currentLocation = currentLocation.move(direction)
+        placesVisited.insert(currentLocation)
+    }
+    
+    return placesVisited.count
+}
