536f3e18c30446e13c4aee40a1db74f5706c386c
Robbie Plankenhorn <rplankenhorn@gmail.com>
2016-05-06 09:03:16 -0500

    Completed Day 15 Part 2

--- AdventOfCode/AdventOfCode/day15.swift
+++ AdventOfCode/AdventOfCode/day15.swift
@@ -35,32 +35,76 @@ func day15Part1() -> Int {
     for i in 1...100 {
         for j in 1...100 {
             for k in 1...100 {
-                for l in 1...100 {
-                    if i + j + k + l == 100 {
-                        var capacity = i*ingredients[0].capacity
-                        var durability = i*ingredients[0].durability
-                        var flavor = i*ingredients[0].flavor
-                        var texture = i*ingredients[0].texture
-                        
-                        capacity += j*ingredients[1].capacity
-                        durability += j*ingredients[1].durability
-                        flavor += j*ingredients[1].flavor
-                        texture += j*ingredients[1].texture
-                        
-                        capacity += k*ingredients[2].capacity
-                        durability += k*ingredients[2].durability
-                        flavor += k*ingredients[2].flavor
-                        texture += k*ingredients[2].texture
-                        
-                        capacity += l*ingredients[3].capacity
-                        durability += l*ingredients[3].durability
-                        flavor += l*ingredients[3].flavor
-                        texture += l*ingredients[3].texture
-                        
-                        let score = Swift.max(capacity, 0) * Swift.max(durability, 0) * Swift.max(flavor, 0) * Swift.max(texture, 0)
-                        
-                        bestScore = Swift.max(bestScore, score)
-                    }
+                let l = 100 - i - j - k
+                
+                var capacity = i*ingredients[0].capacity
+                var durability = i*ingredients[0].durability
+                var flavor = i*ingredients[0].flavor
+                var texture = i*ingredients[0].texture
+                
+                capacity += j*ingredients[1].capacity
+                durability += j*ingredients[1].durability
+                flavor += j*ingredients[1].flavor
+                texture += j*ingredients[1].texture
+                
+                capacity += k*ingredients[2].capacity
+                durability += k*ingredients[2].durability
+                flavor += k*ingredients[2].flavor
+                texture += k*ingredients[2].texture
+                
+                capacity += l*ingredients[3].capacity
+                durability += l*ingredients[3].durability
+                flavor += l*ingredients[3].flavor
+                texture += l*ingredients[3].texture
+                
+                let score = Swift.max(capacity, 0) * Swift.max(durability, 0) * Swift.max(flavor, 0) * Swift.max(texture, 0)
+                
+                bestScore = Swift.max(bestScore, score)
+            }
+        }
+    }
+    
+    return bestScore
+}
+
+func day15Part2() -> Int {
+    let ingredients = readInputFile("day15_input")!.componentsSeparatedByString("\n").map { Ingredient(line: $0) }
+    
+    var bestScore = 0
+    
+    for i in 1...100 {
+        for j in 1...100 {
+            for k in 1...100 {
+                let l = 100 - i - j - k
+                
+                var capacity = i*ingredients[0].capacity
+                var durability = i*ingredients[0].durability
+                var flavor = i*ingredients[0].flavor
+                var texture = i*ingredients[0].texture
+                var calories = i*ingredients[0].calories
+                
+                capacity += j*ingredients[1].capacity
+                durability += j*ingredients[1].durability
+                flavor += j*ingredients[1].flavor
+                texture += j*ingredients[1].texture
+                calories += j*ingredients[1].calories
+                
+                capacity += k*ingredients[2].capacity
+                durability += k*ingredients[2].durability
+                flavor += k*ingredients[2].flavor
+                texture += k*ingredients[2].texture
+                calories += k*ingredients[2].calories
+                
+                capacity += l*ingredients[3].capacity
+                durability += l*ingredients[3].durability
+                flavor += l*ingredients[3].flavor
+                texture += l*ingredients[3].texture
+                calories += l*ingredients[3].calories
+                
+                let score = Swift.max(capacity, 0) * Swift.max(durability, 0) * Swift.max(flavor, 0) * Swift.max(texture, 0)
+                
+                if calories == 500 {
+                    bestScore = Swift.max(bestScore, score)
                 }
             }
         }
