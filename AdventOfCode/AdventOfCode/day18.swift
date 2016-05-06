90650e2fdb7e362225f514fb0ed4cc9844891ba6
Robbie Plankenhorn <rplankenhorn@gmail.com>
2016-05-06 16:28:54 -0500

    Completed Day 18 Part 2

--- AdventOfCode/AdventOfCode/day18.swift
+++ AdventOfCode/AdventOfCode/day18.swift
@@ -8,11 +8,16 @@
 
 import Foundation
 
-private func updateLights(lights:[[Bool]]) -> [[Bool]] {
+private func updateLights(lights:[[Bool]], ignoreCorners:Bool = false) -> [[Bool]] {
     var lightsCopy = lights
     
     for row in 0..<lights.count {
         for column in 0..<lights[row].count {
+            
+            if ignoreCorners && (row == 0 || row == lights.count - 1) && (column == 0 || column == lights.count - 1) {
+                continue
+            }
+            
             var numberOfNeighborsOn = 0
             
             let rowMin = Swift.max(0, row-1)
@@ -40,18 +45,37 @@ private func updateLights(lights:[[Bool]]) -> [[Bool]] {
     return lightsCopy
 }
 
-func day18Part1() -> Int {
+private func getLights() -> [[Bool]] {
     let input = readInputFile("day18_input")!.componentsSeparatedByString("\n")
     
-    var lights:[[Bool]] = input.map { row in
+    return input.map { row in
         let booleanRow = row.characters.map { element in
             return element == "#" ? true : false
         }
         return booleanRow
     }
+}
+
+func day18Part1() -> Int {
+    var lights = getLights()
     
     for _ in 1...100 {
         lights = updateLights(lights)
+    }
+    
+    return lights.map { $0.map { $0 ? 1 : 0 } }.reduce(0, combine: { $0 + $1.reduce(0, combine:+) })
+}
+
+func day18Part2() -> Int {
+    var lights = getLights()
+    
+    lights[0][0] = true
+    lights[lights.count-1][0] = true
+    lights[0][lights.count-1] = true
+    lights[lights.count-1][lights.count-1] = true
+    
+    for _ in 1...100 {
+        lights = updateLights(lights, ignoreCorners: true)
     }
     
     return lights.map { $0.map { $0 ? 1 : 0 } }.reduce(0, combine: { $0 + $1.reduce(0, combine:+) })
