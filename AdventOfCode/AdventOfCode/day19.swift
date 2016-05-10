73894a45b9fbe9eb4398164883cd01f3dcba973c
Robbie Plankenhorn <rplankenhorn@gmail.com>
2016-05-10 10:27:56 -0500

    Completed Day 19 Part 2

--- AdventOfCode/AdventOfCode/day19.swift
+++ AdventOfCode/AdventOfCode/day19.swift
@@ -8,19 +8,19 @@
 
 import Foundation
 
-private func getReplacementsAndMolcule() -> ([String:[String]], String) {
+private func getReplacementsAndMolcule() -> ([String:Set<String>], String) {
     var input = readInputFile("day19_input")!.componentsSeparatedByString("\n")
     
     let molecule = input.removeLast()
     input.removeLast()
     
-    var replacements:[String:[String]] = [:]
+    var replacements:[String:Set<String>] = [:]
     
     for line in input {
         let tokens = line.stringByReplacingOccurrencesOfString(" ", withString: "").componentsSeparatedByString("=>")
         
         var array = replacements[tokens.first!] ?? []
-        array.append(tokens.last!)
+        array.insert(tokens.last!)
         replacements[tokens.first!] = array
     }
     
@@ -48,4 +48,23 @@ func day19Part1() -> Int {
     }
     
     return newMolecules.count
+}
+
+private extension String {
+    
+    func numberOfOccurrancesOfString(str:String) -> Int {
+        let regex = try! NSRegularExpression(pattern: str, options: [])
+        return regex.matchesInString(self, options: [], range: NSMakeRange(0, self.characters.count)).map { $0.range }.count
+    }
+    
+    func stringByRemovingCharactersWithRegexString(str:String) -> String {
+        let regex = try! NSRegularExpression(pattern: str, options: [])
+        return regex.stringByReplacingMatchesInString(self, options: [], range: NSMakeRange(0, self.characters.count), withTemplate: "")
+    }
+}
+
+func day19Part2() -> Int {
+    let replacementsAndMolecule = getReplacementsAndMolcule()
+    let molecule = replacementsAndMolecule.1
+    return molecule.stringByRemovingCharactersWithRegexString("[a-z]").characters.count - molecule.numberOfOccurrancesOfString("Rn") - molecule.numberOfOccurrancesOfString("Ar") - 2 * molecule.numberOfOccurrancesOfString("Y") - 1
 }
